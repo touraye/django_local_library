98c445e799382dd449f33a11f17f658536762a0e
touraye <touraye07@gmail>
2024-07-11 18:44:33 +0000

    added pagination

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -32,6 +32,7 @@ def index(request):
 
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 2
     
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get the context
