b1bca244766e257d6b160d73379ffceee28e56c3
touraye <touraye07@gmail>
2024-07-22 11:42:09 +0000

    site visitor count

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -18,6 +18,10 @@ def index(request):
     
     # Generate counts of some of the main objects
     num_genres = Genre.objects.all().count()
+    
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
 
     context = {
         'num_books': num_books,
@@ -25,6 +29,7 @@ def index(request):
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
         'num_genres': num_genres,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
@@ -52,3 +57,6 @@ class AuthorListView(generic.ListView):
         context = super(AuthorListView, self).get_context_data(**kwargs)
         context['some_data'] = 'This just some data'
         return context
+    
+class AuthorDetailView(generic.ListView):
+    model = Author
