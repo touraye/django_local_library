de83ba1f2d507f6f8ed41d054e58a48817e8cc17
touraye <touraye07@gmail>
2024-07-23 17:39:17 +0000

    update folder structure

--- catalog/models.py
+++ catalog/models.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 import uuid # Required for unique book instances
-
-# Create your models here.
+from django.conf import settings
+from datetime import date
 from django.urls import reverse # Used in get_absolute_url() to get URL for specified ID
 
 from django.db.models import UniqueConstraint # Constrains fields to unique values
@@ -104,6 +105,8 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -122,10 +125,17 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ['due_back']
+        permissions = (("can_mark_returned", "Set book as returned"),)
 
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
+    
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
+
     
     
 class Author(models.Model):
