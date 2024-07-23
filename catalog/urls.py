12e63105ee58c8a5a37b3f9d2ac2e6c1044d496f
touraye <touraye07@gmail>
2024-07-23 17:41:24 +0000

    added new routes

---
+++ catalog/urls.py
@@ -0,0 +1,13 @@
+# Use include() to add paths from the catalog application
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('borrowed/', views.LoanedBooksByLibrarianListView.as_view(), name='borrowed-books'),
+]
