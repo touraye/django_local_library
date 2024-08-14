f153d13dd3bc1d1c8efa67048cb5f82c18711e08
touraye <touraye07@gmail>
2024-08-14 16:56:44 +0000

    update catalog routes

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('register/', views.register, name='register'),
+    path('', views.index, name='index'),    
     path('books/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
