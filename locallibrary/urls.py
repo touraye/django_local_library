88badba4e83a39f58194533c83a50e06034fce54
touraye <touraye07@gmail>
2024-08-14 16:56:23 +0000

    fixed redirect path

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),        
+    path('catalog/', include('catalog.urls')),
+    path('', RedirectView.as_view(url='/catalog')),    
     path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
